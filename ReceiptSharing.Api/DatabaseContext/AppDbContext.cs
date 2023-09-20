beda1ad10bc191c5ef92757fcb571dd6a32c415b
Mathew <mateusz.lu123@gmail.com>
2023-09-20 13:57:18 +0200

    changed connection string

--- ReceiptSharing.Api/DatabaseContext/AppDbContext.cs
+++ ReceiptSharing.Api/DatabaseContext/AppDbContext.cs
@@ -24,7 +24,7 @@ namespace ReceiptSharing.Api.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
             _logger.LogInformation(connectionString);
             optionsBuilder.UseNpgsql(connectionString);
         }
